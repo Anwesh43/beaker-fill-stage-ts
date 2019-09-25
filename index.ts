4e2fe481e23e184ddc1159ec81eefa3e50030a7d
Anwesh43 <anweshthecool0@gmail.com>
2019-09-25 11:07:29 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ const strokeFactor : number = 90
 const delay : number = 50
 const w : number = window.innerWidth
 const h : number = window.innerHeight
+const sizeFactor : number = 5
 
 class BeakerFillStage {
 
@@ -45,5 +46,37 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n
+    }
+}
+
+class DrawingUtil {
+
+    static drawBeaker(context : CanvasRenderingContext2D, hb : number, wb : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.beginPath()
+        context.moveTo(0, -hb)
+        context.lineTo(0, 0)
+        context.lineTo(wb, 0)
+        context.lineTo(wb, -hb)
+        context.stroke()
+    }
+
+    static drawFillRect(context : CanvasRenderingContext2D, sc : number, size : number) {
+        context.fillRect(0, -size * sc, size, size * sc)
+    }
+
+    static drawBFNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, 3)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, 3)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, 3)
+        const size : number = w / sizeFactor
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        context.save()
+        context.translate(-size + (w / 2 + size / 2) * sc1 + (w / 2 - size / 2) * sc3, 9 * h / 10)
+        DrawingUtil.drawBeaker(context, size, 2 * size)
+        DrawingUtil.drawFillRect(context, sc2, size)
+        context.restore()
     }
 }
