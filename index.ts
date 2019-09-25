e1f2ce6f0e0a4178c1efb2f0080a43538c0d887a
Anwesh43 <anweshthecool0@gmail.com>
2019-09-25 11:35:17 +0530

    adding BeakerFill class

--- index.ts
+++ index.ts
@@ -167,3 +167,27 @@ class BFNode {
         return this
     }
 }
+
+class BeakerFill {
+
+    root : BFNode = new BFNode(0)
+    curr : BFNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
